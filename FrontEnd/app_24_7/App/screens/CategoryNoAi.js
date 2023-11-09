33446f7a7aa240c4ac4bbb324edbc4db207bd945
seojuuwon <seojwon127@gmail.com>
2023-11-09 09:17:06 +0900

    주원

---
+++ FrontEnd/app_24_7/App/screens/CategoryNoAi.js
@@ -0,0 +1,87 @@
+import React, {useState} from 'react';
+import {
+  StyleSheet,
+  View,
+  TouchableOpacity,
+  Dimensions,
+  Text,
+} from 'react-native';
+
+// ---------------------------------------------------------
+import Icon from 'react-native-vector-icons/EvilIcons';
+
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+
+const CategoryNoAi = ({navigation}) => {
+
+  React.useLayoutEffect(() => {
+    navigation.setOptions({
+      headerTitle: ' ',
+      headerLeft: ({onPress}) => (
+        <TouchableOpacity
+          onPress={() => {
+            navigation.navigate('Category');
+          }}>
+          <Icon name="chevron-left" size={40} />
+        </TouchableOpacity>
+      ),
+      contentStyle: {
+        backgroundColor: '#FAFAFA',
+      },
+    });
+  }, [navigation]);
+
+  return (
+    <View style={styles.Container}>
+      <TouchableOpacity style = {styles.touchContainer}>
+        <Text style = {styles.headText}>AI 코치와 함께!</Text>
+        <Text style = {styles.text}>AI운동분석결과{"\n"}+{"\n"}트레이너 피드백 제공</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style = {styles.touchContainer}>
+      <Text style = {styles.headText}>부위별hhh 운동</Text>
+        <Text style = {styles.text}>트레이너 피드백 제공</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style = {styles.touchContainer}>
+      <Text style = {styles.headText}>부위별 운동</Text>
+        <Text style = {styles.text}>트레이너 피드백 제공</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  Container: {
+    width: windowWidth,
+    height: windowHeight,
+    backgroundColor: 'white',
+    flex: 1,
+    alignItems : "center",
+    justifyContent : "center"
+  },
+  touchContainer : {
+    backgroundColor : "#F9F7FE",
+    width : windowWidth*0.85,
+    height : windowHeight*0.39,
+    borderRadius : 10,
+    alignItems : "center",
+    justifyContent : "center",
+    marginVertical : 20,
+  },
+  headText : { 
+    fontSize: 30, 
+    fontWeight: "bold", 
+    marginBottom: 10, 
+    color : "black",
+    marginVertical : 10,
+  },
+  text : {
+    textAlign : "center",
+    fontWeight : "bold",
+    color : "black",
+    marginVertical : 20,
+  }, 
+});
+
+export default CategoryNoAi;
---
+++ FrontEnd/app_24_7/App/screens/RecordVideo.js
@@ -0,0 +1,89 @@
+import React, { useState } from 'react';
+import {
+    StyleSheet,
+    View,
+    TouchableOpacity,
+    Dimensions,
+    Text,
+} from 'react-native';
+import { useRoute } from '@react-navigation/native';
+import Video from 'react-native-video';
+// import { RNCamera } from 'react-native-camera';
+// ---------------------------------------------------------
+import Icon from 'react-native-vector-icons/EvilIcons';
+
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+
+const RecordVideo = ({ navigation }) => {
+    const route = useRoute();
+
+  
+
+
+    React.useLayoutEffect(() => {
+        navigation.setOptions({
+            headerTitle: route.params.category,
+            headerLeft: ({ onPress }) => (
+                <TouchableOpacity
+                    onPress={() => {
+                        navigation.navigate('CategoryAi');
+                    }}>
+                    <Icon name="chevron-left" size={40} />
+                </TouchableOpacity>
+            ),
+            contentStyle: {
+                backgroundColor: '#FAFAFA',
+            },
+        });
+    }, [navigation, route]);
+
+    return (
+        <View style={styles.Container}>
+            {/* <Video
+                source={require('../assets/video/squat.mp4')}
+                style={{ width: windowWidth * 0.85, height: windowHeight * 0.4 }}
+                controls={true} // 플레이어 컨트롤 보이기
+                resizeMode="cover" // 동영상 크기 조절
+            /> */}
+            
+
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    Container: {
+        width: windowWidth,
+        height: windowHeight,
+        backgroundColor: 'white',
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    touchContainer: {
+        backgroundColor: "#F9F7FE",
+        width: windowWidth * 0.85,
+        height: windowHeight * 0.39,
+        borderRadius: 10,
+        alignItems: "center",
+        justifyContent: "center",
+        marginVertical: 20,
+    },
+    headText: {
+        fontSize: 30,
+        fontWeight: "bold",
+        marginBottom: 10,
+        color: "black",
+        marginVertical: 10,
+    },
+    text: {
+        textAlign: "center",
+        fontWeight: "bold",
+        color: "black",
+        marginVertical: 20,
+    },
+});
+
+export default RecordVideo;
